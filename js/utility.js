4592544d316762a0f935f714717702797be3c888
staceyjf <staceyfanner@gmail.com>
2024-03-11 21:20:10 +1100

    basic grid responsive and burger menu adds

---
+++ js/utility.js
@@ -0,0 +1,9 @@
+function mobileNavigation() {
+  let x = document.getElementById("navMobile");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
