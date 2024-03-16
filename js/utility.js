1cfa12a4d99d3a310a2cf97860964d6b495cb586
staceyjf <staceyfanner@gmail.com>
2024-03-16 16:58:01 +1100

    JS link add to add and remove active class

--- js/utility.js
+++ js/utility.js
@@ -1,8 +1,8 @@
+//  creating the burger and expanded menu effect
 function mobileNavigation() {
   let burger = document.getElementById("navMobile");
   let bodyWrapper = document.querySelector(".body-wrapper");
 
-  // toggle between adding and removing the 'responsive' and 'menu-open' classes
   if (!burger.classList.contains("responsive")) {
     burger.classList.add("responsive");
     bodyWrapper.classList.add("menu-open");
@@ -11,6 +11,29 @@ function mobileNavigation() {
     bodyWrapper.classList.remove("menu-open");
   }
 }
+
+// top nav bar adjusting the active class based on the sections
+let navLinks = document.querySelectorAll(".top-nav a");
+
+// remove active class for nav
+function removeActiveClass() {
+  navLinks.forEach(function (link) {
+    link.classList.remove("top-nav__link--active");
+  });
+}
+
+// remove active class for nav
+function addActiveClass(link) {
+  link.classList.add("top-nav__link--active");
+}
+
+// Add click event listener to all nav links
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
+    removeActiveClass();
+    addActiveClass(this);
+  });
+});
 
 // swiper.js initalizer taking from the docs
 document.addEventListener("DOMContentLoaded", (event) => {
